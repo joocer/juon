d9800e4d647ef114dcebaa4202ec636d3deb220b
Justin Joyce <justin.joyce@joocer.com>
2020-12-29 09:39:38 +0000

    licencing

--- diablo/__init__.py
+++ diablo/__init__.py
@@ -1,38 +1,54 @@
 """
-Diablo
-
-NetworkX Query Language: Based on Gremlin
+Diablo: NetworkX Query Language: Based on Gremlin
 https://tinkerpop.apache.org/gremlin.html
 
+(C) 2020 Justin Joyce.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 class Diablo(object):
     """
-    diablo: gremlin-like networkx tool
+    Diablo: A NetworkX Query Language
 
-    parameters:
+    Parameters:
     - graph: the graph to query and traverse
     - active_nodes: the nodes which are selected for this instance
     """
 
-    __slots__ = ['graph', 'active_nodes', 'cached_active_nodes', 'edges_cache', 'nodes_cache']
+    # reserve slots for these variables
+    __slots__ = ['graph', 'active_nodes', 'cached_nodes', 'edges_cache', 'nodes_cache']
 
-    def __init__(self, graph, active_nodes=None):
+    def __init__(self, graph, active_nodes: set = ()):
         self.graph = graph
 
-        if active_nodes:
-            # sets are faster to do 'in' against
+        if len(active_nodes) > 0:
+            # ensure it is a set
+            # - the collection active nodes is immutable
+            # - sets are faster for look ups 
             self.active_nodes = set(active_nodes)
         else:
-            # select everything
+            # select everything from the base graph
             self.active_nodes = set(graph.nodes())
-        self.cached_active_nodes = None
-        self.edges_cache = None
-        self.nodes_cache = None
+        
+        # create the variable but don't fill it until used
+        self.cached_nodes = None
 
 
     def V(self, *nodes):
-        
+        """
+        Initialize Diablo against a graph.
+        """
         self.edges_cache = {}
         for x,y,e in self.graph.edges(data=True):
             cache = self.edges_cache.get(e.get('relationship'))
@@ -45,16 +61,16 @@ class Diablo(object):
         return self._is(*nodes)
 
 
-    def __get_active_nodes(self):
+    def __get_cached_nodes(self):
         """
         Get the nodes which are referenced by the cursor
         """
-        if not self.cached_active_nodes:
-            self.cached_active_nodes = {(x,y) for x,y in self.nodes_cache if x in self.active_nodes}
-        return self.cached_active_nodes
+        if not self.cached_nodes:
+            self.cached_nodes = {(x,y) for x,y in self.graph.nodes(data=True) if x in self.active_nodes}
+        return self.cached_nodes
 
 
-    def has(self, key, value):
+    def has(self, key: str, value: str):
         """
         'has' filters graphs by a key/value attribute pairs on nodes.
 
@@ -62,7 +78,7 @@ class Diablo(object):
         - key: node attribute name to filter on
         - value: node attribute value to filter on
 
-        returns diablo instance to enable function chaining
+        returns: new Diablo instance
         """
         active_nodes = {x for x,y in self.nodes_cache if y.get(key) == value}
         newd = Diablo(self.graph, active_nodes)
@@ -97,39 +113,40 @@ class Diablo(object):
         """
         'values' returns a list of values of the selected nodes.
 
-        parameters:
+        Parameters:
         - key: the attribute to read the value from 
 
         returns a list of values
         """
-        return list({y.get(key) for x,y in self.cached_active_nodes()})
+        return list({y.get(key) for x,y in self.__get_cached_nodes()})
 
 
     def groupCount(self, key):
         """
         'groupCount' counts nodes per key attribute
 
-        parameters:
+        Parameters:
         - key: key to group by
 
-        returns a dictionary of counts
+        returns: a dictionary of counts
         """
         from collections import Counter
-        nodes = Counter([y.get(key) for x,y in self.cached_active_nodes()])
+        nodes = Counter([y.get(key) for x,y in self.__get_cached_nodes()])
         return dict(nodes)
+
 
     def _is(self, *identity):
         """
-        'is' selects a single node.
+        'is' explicitly selects nodes.
 
-        parameters:
-        - identity(s): the identity of the node to select
+        Parameters:
+        - identity(s): the identity(s) of the node(s) to select
 
-        returns diablo instance to enable function chaining
+        returns: a new diablo instance
         """
         active_nodes = []
         for ident in identity:
-            if ident in self.nodes():
+            if ident in self.graph.nodes():
                 active_nodes.append(ident)
         newd = Diablo(self.graph, active_nodes)
         newd.nodes_cache = self.nodes_cache
@@ -139,7 +156,7 @@ class Diablo(object):
 
     def nodes(self, data=False):
         """
-        Returns the nodes currently selected
+        Returns the currently selected nodes
         """
         if data:
             return self.__get_cached_nodes()
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="diablo",
-    version="0.0.1",
+    version="0.0.2",
     description="NetworkX Query Language",
     long_description=long_description,
     long_description_content_type="text/markdown",
