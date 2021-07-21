f20b873b5a7ac9f0ad48f8998355e41ceaa0f976
Justin Joyce <justin.joyce@joocer.com>
2021-07-21 16:27:37 +0100

    build

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@ from setuptools import setup, find_packages  # type:ignore
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = eval(open("diablo/version.py", "r").read())  # nosec
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+with open("diablo/version.py", "r") as v:
+    vers = v.read()
+exec(vers)  # nosec
 
 setup(
     name="diablo",
-    version=version,
+    version=__version__,
     description="Python Graph Library",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -15,5 +20,5 @@ setup(
     author_email="justin.joyce@joocer.com",
     packages=find_packages('diablo'),
     url="https://github.com/joocer/diablo",
-    install_requires=[''],
+    install_requires=required,
 )
