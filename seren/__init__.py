f136ab3e51c1c6178dc3b689e260b23e30963e05
Justin Joyce <justin.joyce@joocer.com>
2023-01-14 14:42:06 +0000

    CI

--- seren/__init__.py
+++ seren/__init__.py
@@ -16,7 +16,5 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from seren.dictset import dictset
-from seren.dictset import records
 from seren.graphs.graph import Graph
 from seren.version import __version__
